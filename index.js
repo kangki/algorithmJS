1166a57fe9d9ab0f02e453100d160e0bef42adf8
kangki <xangfi@gmail.com>
2014-09-18 20:16:53 +0900

    소스코드 정리

--- index.js
+++ index.js
@@ -6,60 +6,78 @@ var Queue = require('./Queue');
 var LList = require('./LList');
 
 function head(v){ log.h(v); }
-function print(v,m){
-	log.print('  ', v.toString(), m ? '<<---'+m : '');
-}
+function print(v,m){ log.print('  ', v.toString(), m ? '<<---'+m : ''); }
 
-head([	'-----------------------',
-		' algorithmJS in nodejs ',
-		'  kangki | 2014.09.18  ',
-		'-----------------------']);
+head(['-----------------------',
+			' algorithmJS in nodejs ',
+			'  kangki | 2014.09.18  ',
+			'-----------------------']);
 
 head(['진법변환함수']);
 function mulBase(n, b){
 	if(n==0) return 0;
 
-	var s = new Stack(), r = '';
+	var s = new Stack();
 	while(n > 0){
 		s.push(n % b);
 		n = Math.floor(n / b);
 	}
+
+	var r='';
 	while(s.size()){
 		r += s.pop();
 	}
+
 	return r;
 }
-var n = mulBase(8, 2);	print(n, 'mulBase(8, 2)');
+print(
+	mulBase(8, 2), 
+	'mulBase(8, 2)'
+);
 
 head(['회문확인함수']);
 function roundLetter(w){
-	var s = new Stack(), r = '';
+	var s = new Stack();
 	for(var i=0,n=w.length; i<n; i++){
 		s.push(w[i]);
 	}
+
+	var r = '';
 	while(s.size()){
 		r += s.pop();
 	}
+
 	return w === r;
 }
-var b = roundLetter('ababa'); print(b, 'roundLetter("ababa")');
+print(
+	roundLetter('ababa'),
+	'roundLetter("ababa")'
+);
 
-head(['수열함수']);
+head(['수열함수:재귀']);
 function factorial(n){
 	return n==1 ? 1 : (n * factorial(n-1));
 }
+print(
+	factorial(5),
+	'factorial(5)'
+);
+
+head(['수열함수:스택']);
 function factorial2(n){
-	var s = new Stack(), r=1;
+	var s = new Stack();
 	while(n>0){
 		s.push(n--);
 	}
 	while(s.size()>0){
-		r *= s.pop();
+		n *= s.pop();
 	}
-	return r;
+	return n;
 }
-var d = factorial(5); print(d, 'factorial(5)'),
-	d = factorial2(5); print(d, 'factorial2(5)');
+print(
+	factorial2(5), 
+	'factorial2(5)'
+);
 
 head(['100 미만의 숫자 기수정렬함수']);
 function radixSort(data){
@@ -94,79 +112,114 @@ function radixSort(data){
 
 	distribute(data, qlist, 1);
 	collect(qlist, data);
-
 	distribute(data, qlist, 10);
 	collect(qlist, data);
 
 	return data;
 }
-var l = radixSort([22,41,33,11,12,45,89,19]);	
-print(l, 'radixSort([22,41,33,11,12,45,89,19])');
+print(
+	radixSort([22,41,33,11,12,45,89,19]),
+	'radixSort([22,41,33,11,12,45,89,19])'
+);
 
 ;(function(){
-head(['-- Demo LList(S) --']);
+	head(['-- Demo LList(S) --']);
 
-var ll = new LList();		console.log(ll.toString());
-ll.insert('kim', ll.HEAD); 	console.log(ll.toString());
-ll.insert('kang', 'kim');	console.log(ll.toString());
-ll.insert('hong', 'kang');	console.log(ll.toString());
-ll.insert('ki', 'kang');	console.log(ll.toString());
-ll.remove('ki');			console.log(ll.toString());
+	var ll = new LList();
+	print(ll,'create LList');
 
-head(['-- Demo LList(E) --']);
+	ll.insert('kim', ll.HEAD);
+	print(ll,'insert("kim", ll.HEAD)');
+
+	ll.insert('kang', 'kim');
+	print(ll,'insert("kang","kim")');
+
+	ll.insert('hong', 'kang');
+	print(ll,'insert("hong","kang")');
+
+	ll.insert('ki', 'kang');
+	print(ll,'insert("ki","kang")');
+
+	ll.remove('ki');
+	print(ll,'remove("ki")');
+
+	head(['-- Demo LList(E) --']);
 })//();
 
 ;(function(){
-head(['-- Demo Queue(S) --']);
+	head(['-- Demo Queue(S) --']);
 
-var q = new Queue();	print(q, 'create queue');
-q.enqueue(1);			print(q, 'enqueue(1)');
-q.enqueue(2);			print(q, 'enqueue(2)');
-var v = q.dequeue();	print(q, 'dequeue()='+v);
+	var q = new Queue();
+	print(q, 'create queue');
 
-head(['-- Demo Queue(E) --']);
+	q.enqueue(1);
+	print(q, 'enqueue(1)');
+
+	q.enqueue(2);
+	print(q, 'enqueue(2)');
+
+	var v = q.dequeue(); 
+	print(q, 'dequeue()='+v);
+
+	head(['-- Demo Queue(E) --']);
 })//();
 
 ;(function(){
-head(['-- Demo Stack(S) --']);
+	head(['-- Demo Stack(S) --']);
 
-var s = new Stack();	print(s, 'create stack');
-s.push(1);				print(s, 'push(1)');
-s.push(2);				print(s, 'push(2)');
-var v = s.pop(); 		print(s, '=pop()='+v);
+	var s = new Stack();
+	print(s, 'create stack');
 
-head(['-- Demo Stack(E) --']);
+	s.push(1);
+	print(s, 'push(1)');
+
+	s.push(2);
+	print(s, 'push(2)');
+
+	var v = s.pop();
+	print(s, 'pop()='+v);
+
+	head(['-- Demo Stack(E) --']);
 })//();
 
 ;(function(){
-head(['-- Demo List(S) --']);
+	head(['-- Demo List(S) --']);
 
-var l = new List();	print(l, 'create list');
-l.add(1); 			print(l, 'add(1)');
-l.insert(0,2);		print(l, 'insert(0,2)');
-l.insert(0,4);		print(l, 'insert(0,4)');
-l.remove(0);		print(l, 'remove(0)');
+	var l = new List();	
+	print(l, 'create list');
 
-print(l.indexOf(1), 	'indexOf(1)');
-print(l.contains(1), 	'contains(1)');
+	l.add(1);
+	print(l, 'add(1)');
 
-for(var i=0,n=l.size();i<n;i++) {
-	var v = l.get(i);
-	l.set(i, v+5);
-	print(v+'->'+l.get(i), 'get('+i+'), set('+i+','+l.get(i)+')');
-} 
+	l.insert(0,2);
+	print(l, 'insert(0,2)');
 
-print(l.indexOf(1), 	'indexOf(1)');
-print(l.contains(1), 	'contains(1)');
+	l.insert(0,4);
+	print(l, 'insert(0,4)');
 
-print(l.size(), 	'size()');
-print(l.empty(), 	'empty()');
+	l.remove(0);
+	print(l, 'remove(0)');
 
-l.clear();			print(l, 'clear()');
+	print(l.indexOf(1), 'indexOf(1)');
+	print(l.contains(1), 'contains(1)');
 
-print(l.size(), 	'size()');
-print(l.empty(), 	'empty()');
+	for(var i=0,n=l.size();i<n;i++) {
+		var v = l.get(i);
+		l.set(i, v+5);
+		print(v+'->'+l.get(i), 'get('+i+'), set('+i+','+l.get(i)+')');
+	} 
 
-head(['-- Demo List(E) --']);
+	print(l.indexOf(1), 'indexOf(1)');
+	print(l.contains(1), 'contains(1)');
+
+	print(l.size(), 'size()');
+	print(l.empty(), 'empty()');
+
+	l.clear();
+	print(l, 'clear()');
+	print(l.size(), 'size()');
+	print(l.empty(), 'empty()');
+
+	head(['-- Demo List(E) --']);
 })//();
 

39ca741696e93fc0d4a04c11a10e258df3302672
kangki <xangfi@gmail.com>
2014-10-02 17:09:11 +0900

    node server , index.html angularjs 적용

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 var http = require('http');
+var path = require('path');
 var url = require('url');
+var fs = require('fs');
 
-var request = function(request, response){
+http.createServer(function(request,response){
 	var pathname = url.parse(request.url).pathname;
 
-	response.writeHead(200, {'Content-Type':'text/plain'});
-	response.write('hello node');
-	response.end();
-};
+	var ext = pathname.split('.');
+	ext = ext[ext.length-1];
+	ext = ext || 'plain';
 
-http.createServer(request).listen(8888);
+	var fullpath = path.join(process.cwd(), pathname);
+
+	console.log('pathname:' + pathname);
+
+	try{
+		var status = fs.statSync(fullpath);
+		if(!status.isFile()){
+			response.writeHeader(404, {'Content-Type':'text/plain'});
+			response.write('404 Not Found\n');
+			response.end();
+			return;
+		}
+	}catch(e){
+		response.writeHeader(404, {'Content-Type':'text/plain'});
+		response.write('404 Not Found\n');
+		response.end();
+		console.log(e);
+		return;
+	}
+
+	fs.readFile(fullpath, 'binary', function(err, file){
+		if(err){
+				response.writeHeader(500, {'Content-Type':'text/plain'});
+				response.write(err + '\n');
+				response.end();
+			return;
+		}
+
+		response.writeHeader(200, {'Content-Type':'text/'+ext});
+		response.write(file, 'binary');
+		response.end();
+	});
+
+}).listen(8888);
 
 console.log('Server running');
